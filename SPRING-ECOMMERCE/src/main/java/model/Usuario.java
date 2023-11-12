053032c0a71d96286ef1557c0bfebdb9292f7f41
Juan Agustin Saba <jsaba543@gmail.com>
2023-11-12 18:41:30 -0300

    Update Usuario.java
    Agregamos el código

--- SPRING-ECOMMERCE/src/main/java/model/Usuario.java
+++ SPRING-ECOMMERCE/src/main/java/model/Usuario.java
@@ -1,1 +1,218 @@
+package com.utn.ecommerce.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String nombre;
+	private String username;
+	private String mail;
+	private String direccion;
+	private String dni;
+	private String telefono;
+	private String tipo;
+	private String password;
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto> productos;
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden> ordenes;
+
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(Integer id, String nombre, String username, String mail, String direccion, String dni,
+			String telefono, String tipo, String password, List<Producto> productos, List<Orden> ordenes) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.username = username;
+		this.mail = mail;
+		this.direccion = direccion;
+		this.dni = dni;
+		this.telefono = telefono;
+		this.tipo = tipo;
+		this.password = password;
+		this.productos = productos;
+		this.ordenes = ordenes;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the mail
+	 */
+	public String getMail() {
+		return mail;
+	}
+
+	/**
+	 * @param mail the mail to set
+	 */
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	/**
+	 * @return the direccion
+	 */
+	public String getDireccion() {
+		return direccion;
+	}
+
+	/**
+	 * @param direccion the direccion to set
+	 */
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	/**
+	 * @return the dni
+	 */
+	public String getDni() {
+		return dni;
+	}
+
+	/**
+	 * @param dni the dni to set
+	 */
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	/**
+	 * @return the telefono
+	 */
+	public String getTelefono() {
+		return telefono;
+	}
+
+	/**
+	 * @param telefono the telefono to set
+	 */
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	/**
+	 * @return the tipo
+	 */
+	public String getTipo() {
+		return tipo;
+	}
+
+	/**
+	 * @param tipo the tipo to set
+	 */
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	/**
+	 * @return the productos
+	 */
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	/**
+	 * @param productos the productos to set
+	 */
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+
+	/**
+	 * @return the ordenes
+	 */
+	public List<Orden> getOrdenes() {
+		return ordenes;
+	}
+
+	/**
+	 * @param ordenes the ordenes to set
+	 */
+	public void setOrdenes(List<Orden> ordenes) {
+		this.ordenes = ordenes;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", username=" + username + ", mail=" + mail + ", direccion="
+				+ direccion + ", dni=" + dni + ", telefono=" + telefono + ", tipo=" + tipo + ", password=" + password
+				+ ", productos=" + productos + ", ordenes=" + ordenes + ", getPassword()=" + getPassword()
+				+ ", getId()=" + getId() + ", getNombre()=" + getNombre() + ", getUsername()=" + getUsername()
+				+ ", getMail()=" + getMail() + ", getDireccion()=" + getDireccion() + ", getDni()=" + getDni()
+				+ ", getTelefono()=" + getTelefono() + ", getTipo()=" + getTipo() + ", getProductos()=" + getProductos()
+				+ ", getOrdenes()=" + getOrdenes() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+
+}
 
