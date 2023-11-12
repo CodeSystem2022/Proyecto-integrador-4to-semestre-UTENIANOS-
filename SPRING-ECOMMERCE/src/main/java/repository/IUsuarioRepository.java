7b180a132cd68f3b1bf7deae2bf22574450de3ef
Miriam <111610111+MiriamTorres30@users.noreply.github.com>
2023-11-12 19:30:14 -0300

    IUsuarioRepository.java

--- SPRING-ECOMMERCE/src/main/java/repository/IUsuarioRepository.java
+++ SPRING-ECOMMERCE/src/main/java/repository/IUsuarioRepository.java
@@ -1,1 +1,13 @@
+package com.utn.ecommerce.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.utn.ecommerce.model.Usuario;
+
+@Repository
+public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+	Optional<Usuario> findByMail(String mail);
+}
