2c339dbf5576e13d8a6f51710a4dd4c6f7dcb1df
Miriam <111610111+MiriamTorres30@users.noreply.github.com>
2023-11-12 19:28:51 -0300

    IOrdenRepository.java

--- SPRING-ECOMMERCE/src/main/java/repository/IOrdenRepository.java
+++ SPRING-ECOMMERCE/src/main/java/repository/IOrdenRepository.java
@@ -1,1 +1,16 @@
+package com.utn.ecommerce.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.utn.ecommerce.model.Orden;
+import com.utn.ecommerce.model.Usuario;
+
+@Repository
+public interface IOrdenRepository extends JpaRepository<Orden, Integer> {
+
+	List<Orden> findByUsuario(Usuario usuario);
+
+}
