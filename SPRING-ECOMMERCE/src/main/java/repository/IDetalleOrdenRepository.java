13386db416af7c193e82ddb7fcb1ecfb4dfa4f38
Miriam <111610111+MiriamTorres30@users.noreply.github.com>
2023-11-12 19:28:09 -0300

    IDetalleOrdenRepository.java

--- SPRING-ECOMMERCE/src/main/java/repository/IDetalleOrdenRepository.java
+++ SPRING-ECOMMERCE/src/main/java/repository/IDetalleOrdenRepository.java
@@ -1,1 +1,11 @@
+package com.utn.ecommerce.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.utn.ecommerce.model.DetalleOrden;
+
+@Repository
+public interface IDetalleOrdenRepository extends JpaRepository<DetalleOrden, Integer> {
+
+}
