8146d1a6506bd8e9c795608d8f5affa32de3dcb1
David <111609786+DavidTorres92@users.noreply.github.com>
2023-11-12 18:59:21 -0300

    SpringEcommerceApplication.java

--- SPRING-ECOMMERCE/src/main/java/service/SpringEcommerceApplication.java
+++ SPRING-ECOMMERCE/src/main/java/service/SpringEcommerceApplication.java
@@ -1,1 +1,13 @@
+package com.utn.ecommerce;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommerceApplication.class, args);
+	}
+
+}
