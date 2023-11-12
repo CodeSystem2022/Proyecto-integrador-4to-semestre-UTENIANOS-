54a4f20e3742b1f31ab6ac053d2fbd2af520f19b
David <111609786+DavidTorres92@users.noreply.github.com>
2023-11-12 19:07:51 -0300

    ProductoServicelmpl.java

---
+++ SPRING-ECOMMERCE/src/main/java/service/ProductoServicelmpl.java
@@ -0,0 +1,48 @@
+package com.utn.ecommerce.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.utn.ecommerce.model.Producto;
+import com.utn.ecommerce.repository.IProductoRepository;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+	@Autowired
+	private IProductoRepository productoRepository;
+
+	@Override
+	public Producto save(Producto producto) {
+
+		return productoRepository.save(producto);
+	}
+
+	@Override
+	public Optional<Producto> get(Integer id) {
+
+		return productoRepository.findById(id);
+	}
+
+	@Override
+	public void update(Producto producto) {
+		productoRepository.save(producto);
+
+	}
+
+	@Override
+	public void delete(Integer id) {
+		productoRepository.deleteById(id);
+
+	}
+
+	@Override
+	public List<Producto> findAll() {
+
+		return productoRepository.findAll();
+	}
+
+}
