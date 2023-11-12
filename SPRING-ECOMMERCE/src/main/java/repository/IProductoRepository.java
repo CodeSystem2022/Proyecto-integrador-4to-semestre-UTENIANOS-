93b7a4749408628793cd296d47b98411f87ae036
Miriam <111610111+MiriamTorres30@users.noreply.github.com>
2023-11-12 19:29:31 -0300

    IProductoRepository.java

--- SPRING-ECOMMERCE/src/main/java/repository/IProductoRepository.java
+++ SPRING-ECOMMERCE/src/main/java/repository/IProductoRepository.java
@@ -1,1 +1,11 @@
+package com.utn.ecommerce.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.utn.ecommerce.model.Producto;
+
+@Repository
+public interface IProductoRepository extends JpaRepository<Producto, Integer> {
+
+}
