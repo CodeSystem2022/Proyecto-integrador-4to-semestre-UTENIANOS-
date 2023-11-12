532ac54910955e33e3186f9df6a1a39f8ed55b05
Juan Agustin Saba <jsaba543@gmail.com>
2023-11-12 18:40:23 -0300

    Update Orden.java
    Agregamos el código

--- SPRING-ECOMMERCE/src/main/java/model/Orden.java
+++ SPRING-ECOMMERCE/src/main/java/model/Orden.java
@@ -1,1 +1,152 @@
+package com.utn.ecommerce.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ordenes")
+public class Orden {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String numero;
+	private Date fechaCreacion;
+	private Date fechaRecibida;
+	private double total;
+
+	@ManyToOne
+	private Usuario usuario;
+
+	@OneToMany(mappedBy = "orden")
+	private List<DetalleOrden> detalle;
+
+	public Orden() {
+		super();
+	}
+
+	public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida, double total, Usuario usuario,
+			List<DetalleOrden> detalle) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		this.fechaCreacion = fechaCreacion;
+		this.fechaRecibida = fechaRecibida;
+		this.total = total;
+		this.usuario = usuario;
+		this.detalle = detalle;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the numero
+	 */
+	public String getNumero() {
+		return numero;
+	}
+
+	/**
+	 * @param numero the numero to set
+	 */
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	/**
+	 * @return the fechaCreacion
+	 */
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	/**
+	 * @param fechaCreacion the fechaCreacion to set
+	 */
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	/**
+	 * @return the fechaRecibida
+	 */
+	public Date getFechaRecibida() {
+		return fechaRecibida;
+	}
+
+	/**
+	 * @param fechaRecibida the fechaRecibida to set
+	 */
+	public void setFechaRecibida(Date fechaRecibida) {
+		this.fechaRecibida = fechaRecibida;
+	}
+
+	/**
+	 * @return the total
+	 */
+	public double getTotal() {
+		return total;
+	}
+
+	/**
+	 * @param total the total to set
+	 */
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	/**
+	 * @return the usuario
+	 */
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	/**
+	 * @param usuario the usuario to set
+	 */
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	/**
+	 * @return the detalle
+	 */
+	public List<DetalleOrden> getDetalle() {
+		return detalle;
+	}
+
+	/**
+	 * @param detalle the detalle to set
+	 */
+	public void setDetalle(List<DetalleOrden> detalle) {
+		this.detalle = detalle;
+	}
+
+	@Override
+	public String toString() {
+		return "Orden [id=" + id + ", numero=" + numero + ", fechaCreacion=" + fechaCreacion + ", fechaRecibida="
+				+ fechaRecibida + ", total=" + total + ", usuario=" + usuario + ", detalle=" + detalle + "]";
+	}
+
+}
 
