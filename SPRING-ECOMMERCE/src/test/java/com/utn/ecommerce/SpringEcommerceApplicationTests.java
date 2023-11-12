102465e3a3b13f13abe4d39f77ef2b4755786c96
Diego Agustin Putrino Chirivino <85753366+agussputrii@users.noreply.github.com>
2023-11-12 19:42:33 -0300

    Update SpringEcommerceApplicationTests.java

--- SPRING-ECOMMERCE/src/test/java/com/utn/ecommerce/SpringEcommerceApplicationTests.java
+++ SPRING-ECOMMERCE/src/test/java/com/utn/ecommerce/SpringEcommerceApplicationTests.java
@@ -1,1 +1,13 @@
+package com.utn.ecommerce;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
