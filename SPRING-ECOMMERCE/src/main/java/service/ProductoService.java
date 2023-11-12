c16633fdc964323a485473a57c046443b104d774
David <111609786+DavidTorres92@users.noreply.github.com>
2023-11-12 19:08:48 -0300

    Create ProductoService.java

---
+++ SPRING-ECOMMERCE/src/main/java/service/ProductoService.java
@@ -0,0 +1,20 @@
+package com.utn.ecommerce.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.utn.ecommerce.model.Producto;
+
+public interface ProductoService {
+
+	public Producto save(Producto producto);
+
+	public Optional<Producto> get(Integer id);
+
+	public void update(Producto producto);
+
+	public void delete(Integer id);
+
+	public List<Producto> findAll();
+
+}
