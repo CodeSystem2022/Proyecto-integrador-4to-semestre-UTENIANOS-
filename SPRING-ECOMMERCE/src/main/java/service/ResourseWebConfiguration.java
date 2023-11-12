10bea70bc809e3c72fe2ce6a5d9aac3c8f5feac6
David <111609786+DavidTorres92@users.noreply.github.com>
2023-11-12 18:58:17 -0300

    ResourseWebConfiguration.java

--- SPRING-ECOMMERCE/src/main/java/service/ResourseWebConfiguration.java
+++ SPRING-ECOMMERCE/src/main/java/service/ResourseWebConfiguration.java
@@ -1,1 +1,13 @@
+package com.utn.ecommerce;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceWebConfiguration implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
+	}
+}
