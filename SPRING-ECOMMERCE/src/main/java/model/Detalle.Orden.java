3226245e61e923463276ac717ddcdb89b50ae0f2
Juan Agustin Saba <jsaba543@gmail.com>
2023-11-12 18:39:48 -0300

    Update Detalle.Orden.java
    Agregamos el código

--- SPRING-ECOMMERCE/src/main/java/model/Detalle.Orden.java
+++ SPRING-ECOMMERCE/src/main/java/model/Detalle.Orden.java
@@ -1,1 +1,148 @@
+package com.utn.ecommerce.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "detalles")
+public class DetalleOrden {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private double cantidad;
+	private double precio;
+	private double total;
+
+	@ManyToOne
+	private Orden orden;
+
+	@ManyToOne
+	private Producto producto;
+
+	public DetalleOrden() {
+		super();
+	}
+
+	public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total, Orden orden,
+			Producto producto) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		this.precio = precio;
+		this.total = total;
+		this.orden = orden;
+		this.producto = producto;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the cantidad
+	 */
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	/**
+	 * @param cantidad the cantidad to set
+	 */
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	/**
+	 * @return the precio
+	 */
+	public double getPrecio() {
+		return precio;
+	}
+
+	/**
+	 * @param precio the precio to set
+	 */
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	/**
+	 * @return the total
+	 */
+	public double getTotal() {
+		return total;
+	}
+
+	/**
+	 * @param total the total to set
+	 */
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	/**
+	 * @return the orden
+	 */
+	public Orden getOrden() {
+		return orden;
+	}
+
+	/**
+	 * @param orden the orden to set
+	 */
+	public void setOrden(Orden orden) {
+		this.orden = orden;
+	}
+
+	/**
+	 * @return the producto
+	 */
+	public Producto getProducto() {
+		return producto;
+	}
+
+	/**
+	 * @param producto the producto to set
+	 */
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	@Override
+	public String toString() {
+		return "DetalleOrden [id=" + id + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio
+				+ ", total=" + total + ", orden=" + orden + ", producto=" + producto + "]";
+	}
+
+}
 
